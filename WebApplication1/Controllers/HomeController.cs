b6e1d5f58122382847e72267821fad1a88fcf943
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2023-04-18 17:04:30 +0400

    Add files via upload

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Contact() 
+        { 
+            return View(); 
+        }
+        public ViewResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController:Controller
+    {
+        //public ContentResult Detail() { 
+        //ContentResult result = new ContentResult();
+
+        //result.Content = "Product detial";
+
+        //return result;
+        //}
+
+        //public JsonResult Detail()
+        //{
+        //    JsonResult result = new JsonResult(new { Name = "qwer", Price = 23 });
+        //    return result;
+        //}
+
+        public ContentResult Detail(int id)
+        {
+        //    ViewResult result = new ViewResult();
+        //    result.ViewName = "Detail";
+        //    return result;
+            return Content(id.ToString());
+        }
+
+
+        public ViewResult Index()
+        {
+            //ViewResult result = new ViewResult();
+            //result.ViewName = "Index";
+
+            var result = View();
+
+            return result;
+        }
+
+
+    }
+}
